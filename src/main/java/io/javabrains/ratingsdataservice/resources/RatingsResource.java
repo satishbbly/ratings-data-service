3a44deb47fed482eaa4f7bb1132d50b7d948a8cc
Satish Kumar <satishbbly@gmail.com>
2019-11-27 00:28:42 +0530

    sleuth and logging added

--- src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
+++ src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
@@ -1,5 +1,8 @@
 package io.javabrains.ratingsdataservice.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +14,19 @@ import io.javabrains.ratingsdataservice.model.UserRating;
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
 
+	
+	private Logger logger=LoggerFactory.getLogger(RatingsResource.class);
+	
     @RequestMapping(value="/movies/{movieId}" )
 
     public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+    	logger.info("in getMovieRating");
         return new Rating(movieId, 4);
     }
 
     @RequestMapping("/user/{userId}")
     public UserRating getUserRatings(@PathVariable("userId") String userId) {
+    	logger.info("in getUserRatings");
         UserRating userRating = new UserRating();
         userRating.initData(userId);
         return userRating;
