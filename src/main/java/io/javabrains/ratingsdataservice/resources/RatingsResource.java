8fe28d0349be7041dc958c77706e6579ff44e6c3
Satish Kumar <satishbbly@gmail.com>
2019-11-26 20:49:10 +0530

    some modification

--- src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
+++ src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
@@ -1,16 +1,18 @@
 package io.javabrains.ratingsdataservice.resources;
 
-import io.javabrains.ratingsdataservice.model.Rating;
-import io.javabrains.ratingsdataservice.model.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.ratingsdataservice.model.Rating;
+import io.javabrains.ratingsdataservice.model.UserRating;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
 
-    @RequestMapping("/movies/{movieId}")
+    @RequestMapping(value="/movies/{movieId}" )
+
     public Rating getMovieRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
     }
